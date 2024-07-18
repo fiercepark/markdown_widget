674bf4c5cbc9bb9abfa8e898ad078a9bf0424bc0
Siegfried <fiercepark.uk@gmail.com>
2024-07-18 11:40:06 +0800

    fixup! Remove white space

--- lib/widget/blocks/leaf/link.dart
+++ lib/widget/blocks/leaf/link.dart
@@ -6,61 +6,6 @@ import 'package:url_launcher/url_launcher.dart';
 import '../../../config/configs.dart';
 import '../../span_node.dart';
 
-///Tag: [MarkdownTag.a]
-///
-///Link reference definitions, A link reference definition consists of a link label
-///link will always be wrapped by other tags, such as [MarkdownTag.p]
-
-class LinkNode extends ElementNode {
-  final Map<String, String> attributes;
-  final LinkConfig linkConfig;
-
-  LinkNode(this.attributes, this.linkConfig);
-
-  @override
-  InlineSpan build() {
-    final url = attributes['href'] ?? '';
-    return TextSpan(children: [
-      for (final child in children)
-        _toLinkInlineSpan(
-          child.build(),
-          () => _onLinkTap(linkConfig, url),
-        ),
-      if (children.isNotEmpty)
-        // FIXME: this is a workaround, maybe need fixed by flutter framework.
-        // add a space to avoid the space area of line end can be tapped.
-        TextSpan(text: ' '),
-    ]);
-  }
-
-  void _onLinkTap(LinkConfig linkConfig, String url) {
-    if (linkConfig.onTap != null) {
-      linkConfig.onTap?.call(url);
-    } else {
-      launchUrl(Uri.parse(url));
-    }
-  }
-
-  @override
-  TextStyle get style =>
-      parentStyle?.merge(linkConfig.style) ?? linkConfig.style;
-}
-
-///config class for link, tag: a
-class LinkConfig implements LeafConfig {
-  final TextStyle style;
-  final ValueCallback<String>? onTap;
-
-  const LinkConfig(
-      {this.style = const TextStyle(
-          color: Color(0xff0969da), decoration: TextDecoration.underline),
-      this.onTap});
-
-  @nonVirtual
-  @override
-  String get tag => MarkdownTag.a.name;
-}
-
 // add a tap gesture recognizer to the span.
 InlineSpan _toLinkInlineSpan(InlineSpan span, VoidCallback onTap) {
   if (span is TextSpan) {
@@ -87,4 +32,55 @@ InlineSpan _toLinkInlineSpan(InlineSpan span, VoidCallback onTap) {
     );
   }
   return span;
+}
+
+///config class for link, tag: a
+class LinkConfig implements LeafConfig {
+  final TextStyle style;
+  final ValueCallback<String>? onTap;
+
+  const LinkConfig(
+      {this.style = const TextStyle(
+          color: Color(0xff0969da), decoration: TextDecoration.underline),
+      this.onTap});
+
+  @nonVirtual
+  @override
+  String get tag => MarkdownTag.a.name;
+}
+
+///Tag: [MarkdownTag.a]
+///
+///Link reference definitions, A link reference definition consists of a link label
+///link will always be wrapped by other tags, such as [MarkdownTag.p]
+
+class LinkNode extends ElementNode {
+  final Map<String, String> attributes;
+  final LinkConfig linkConfig;
+
+  LinkNode(this.attributes, this.linkConfig);
+
+  @override
+  TextStyle get style =>
+      parentStyle?.merge(linkConfig.style) ?? linkConfig.style;
+
+  @override
+  InlineSpan build() {
+    final url = attributes['href'] ?? '';
+    return TextSpan(children: [
+      for (final child in children)
+        _toLinkInlineSpan(
+          child.build(),
+          () => _onLinkTap(linkConfig, url),
+        ),
+    ]);
+  }
+
+  void _onLinkTap(LinkConfig linkConfig, String url) {
+    if (linkConfig.onTap != null) {
+      linkConfig.onTap?.call(url);
+    } else {
+      launchUrl(Uri.parse(url));
+    }
+  }
 }
